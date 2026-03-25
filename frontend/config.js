cd6c7c8bda3a3f6e10753bba32a1429d0047ddab
Preetithapa26 <25bcab49@kristujanati.com>
2026-03-25 09:38:59 +0530

    Backend deployment successful

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
 const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? 'http://localhost:3000/expenses'
-    : 'https://YOUR_BACKEND_RENDER_URL.onrender.com/expenses'; // Replace prior to GitHub frontend deployment
+    : 'https://student-expense-manager.onrender.com/expenses'; // Replace prior to GitHub frontend deployment
